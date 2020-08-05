7ea9325abe970f60648cab5b3b089ac9e3d59337
albertodelviejo <albertodelviejosanchez@gmail.com>
2020-08-05 10:53:38 +0200

    Creacion del proyecto

---
+++ lib/description_place.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+  @override
+  Widget build(BuildContext context) {
+    final star_half = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(Icons.star_half, color: Color(0xFFf2C611)),
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(Icons.star_border, color: Color(0xFFf2C611)),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(Icons.star, color: Color(0xFFf2C611)),
+    );
+
+    final description_text = Container(
+      child: Text(
+        descriptionPlace,
+        style: TextStyle(
+            color: Color(0xFF56575a),
+            fontSize: 16,
+            fontWeight: FontWeight.bold),
+      ),
+      margin: EdgeInsets.only(left: 20, top: 15, right: 20),
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            namePlace,
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: <Widget>[star, star, star, star, star_border],
+        )
+      ],
+    );
+
+    final description_place = Column(
+      children: <Widget>[title_stars, description_text],
+    );
+    return description_place;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/description_place.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  String dummyText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Platzi Trips',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Platzi"),
+          ),
+          body: new DescriptionPlace("Bahamas", 4, dummyText),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
